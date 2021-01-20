62e61abbb5a09b3cdc62d8a6ab84fc8e206e6f47
Natalia <egisto.siraghi@gmail.com>
2021-01-20 22:06:53 +0100

    altro

---
+++ cypress/integration/testWithPageObjects.js
@@ -0,0 +1,21 @@
+import { onFormLayoutsPage } from "../support/page_objects/formLayoutsPage"
+import { navigateTo } from "../support/page_objects/navigationPage"
+
+describe('Test with Page Objects', () => {
+    beforeEach('open application',  () =>  {
+        cy.openHomePage()
+    })
+
+    it('verify navigation across the pages', ()  => {
+        navigateTo.formLayoutsPage()
+        navigateTo.datepickerPage()
+        navigateTo.smartTablePage()
+        navigateTo.tooltipPage()
+        navigateTo.toasterPage()
+    })
+
+    it('should submit inline and basic form and select tomorrow date in the calendar', () => {
+        navigateTo.formLayoutsPage()
+        onFormLayoutsPage.submitInlineFormWithNameAndEmail('Artem', 'test@test.com')
+    })
+})
